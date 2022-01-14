4ac03d958ccb5c0b925c99e3258cbf9c7c774af0
Rutik Chaudhari <chaudharihrutik@gmail.com>
2022-01-14 17:23:14 +0530

    Add files via upload

---
+++ memberdetails.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE memberdetails
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [city] VARCHAR(50) NULL, 
+    [email] VARCHAR(50) NULL, 
+    [phone] VARCHAR(50) NULL, 
+    [dob] DATE NULL, 
+    [package] VARCHAR(50) NULL
+)
